6f61d261e2f403702b94f017ba136c865dfde79e
tripleslash <development.slash@gmail.com>
2018-04-27 00:11:27 +0200

    Wrote an API wrapper for .NET

---
+++ msvc/tracer_gui/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace tracer_gui
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static readonly tracer_wrapper.ApiWrapper TracerApi = new tracer_wrapper.ApiWrapper();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(TracerApi.LastErrorMessage);
+        }
+    }
+}
